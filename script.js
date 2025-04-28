59ae1e25206778a32b3a07d6db2d45dfcd35c716
Rein Mark <reinmark.delrosario@gmail.com>
2025-04-28 20:55:39 +0800

    add gradient, style buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+// const track1 = document.getElementById("track1")
+// const track2 = document.getElementById("track2")
+// const track3 = document.getElementById("track3")
+// const track4 = document.getElementById("track4")
+// const track5 = document.getElementById("track5")
+
+// let track1audio = document.getElementById("track1audio")
+
+// function playMe() {
+//     trackaudio.play() ;
+// }
