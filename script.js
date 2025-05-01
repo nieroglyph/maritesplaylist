d74b31891758dad0edb486c535051043783334ea
Rein Mark <reinmark.delrosario@gmail.com>
2025-05-01 18:23:18 +0800

    fixes, about.css

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-let isPlaying = false;
-let shuffled = false;
-
 let curr_track = document.createElement('audio');
 
 let track_list = [
     {
         name: "ASAP",
         artist: "NewJaans",
-        image: "./img/newjeans-2ndep.jpeg",
-        path: "./audio/asap.mp3",
+        image: "/img/newjeans-2ndep.jpeg",
+        path: "/audio/asap.mp3",
     },
     {
         name: "Ditto",
         artist: "NewJaans",
-        image: "./img/ditto.jpeg",
-        path: "./audio/ditto.mp3",
+        image: "/img/ditto.jpeg",
+        path: "/audio/ditto.mp3",
     },
     {
         name: "OMG",
         artist: "NewJaans",
-        image: "./img/omg.jpeg",
-        path: "./audio/omg.mp3",
+        image: "/img/omg.jpeg",
+        path: "/audio/omg.mp3",
     },
     {
         name: "ETA",
         artist: "NewJaans",
-        image: "./img/newjeans-2ndep.jpeg",
-        path: "./audio/eta.mp3",
+        image: "/img/newjeans-2ndep.jpeg",
+        path: "/audio/eta.mp3",
     },
     {
         name: "Hype Boy",
         artist: "NewJaans",
-        image: "./img/hypeboy.jpeg",
-        path: "./audio/hypeboy.mp3",
+        image: "/img/hypeboy.jpeg",
+        path: "/audio/hypeboy.mp3",
     },
   ];
+
+let isPlaying = false;
+let shuffled = false;
+let test = null;
 
 const trackButtons = document.querySelectorAll(".trackbutton");
 const nowPlayingIcon = document.getElementById('now-playing-icon');
@@ -44,51 +45,58 @@ const outPlayButton = document.getElementById('outbutton-play');
 const followButton = document.getElementById("outbutton-follow");
 const shuffleButton = document.getElementById("outbutton-shuffle");
 
-let trackId;
-let trackName;
-let trackImg;
-trackButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      const buttonId = this.id;
+// strips the file path
+function getRelativePath(absolutePath) {
+    const basePath = window.location.origin; 
+    return absolutePath.replace(basePath, ''); 
+  }
 
-      for (let x in track_list) {
-        if (buttonId == track_list[x].name) {
-          trackId = track_list[x].path;
-          trackName = track_list[x].name;
-          trackImg = track_list[x].image;
-        }
-      }
+function loadTrack (index, button) {
+    const track = track_list[index];
 
-      if (curr_track.name != trackName) {       // assign attributes if new track is clicked
-          curr_track.src = trackId;     // assign path to audio src
-          curr_track.name = trackName;      // assign track name for checks
-      }
-      
-      if (isPlaying == false) {
-          playTrack();
-          this.innerHTML = '<ion-icon name="pause"></ion-icon>';
-      }
-      else {
-          pauseTrack();
-          this.innerHTML = '<ion-icon name="play"></ion-icon>';
-      }
-    });
-  });
+    if (getRelativePath(curr_track.src) === track.path) {
+        if (!isPlaying) playTrack();
+        else pauseTrack();
+
+    } else {
+        pauseTrack();
+
+        curr_track.src = track.path;
+        if (!isPlaying) playTrack();
+    }
+    updateMedia(track);
+    updateButtons(button);
+}
 
 function playTrack() {
     curr_track.play();
     isPlaying = true;
-
-    nowPlayingIcon.src = trackImg;      // change bottom left icon
-    nowPlayingTitle.innerHTML = trackName;      // change bottom left icon
 }
-
 function pauseTrack() {
-    curr_track.pause();
+    curr_track.pause()
     isPlaying = false;
 }
 
-// out buttons
+function updateMedia(track) {
+    nowPlayingIcon.src = track.image;      // change bottom left icon
+    nowPlayingTitle.innerHTML = track.name;      // change artist name
+}
+function updateButtons(button) {
+    if (isPlaying) {
+        button.innerHTML = '<ion-icon name="pause"></ion-icon>';
+    } else {
+        button.innerHTML = '<ion-icon name="play"></ion-icon>';
+    }
+}
+
+// for each buttons, if clicked, play track with the button index
+trackButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        test = index;
+        loadTrack(index, button);
+    });
+});
+
 function followed() {
     if (followButton.innerHTML == "Follow") {
         followButton.innerHTML = "Following";
@@ -105,20 +113,44 @@ function shuffle() {
         shuffled = false;
     }
 }
-function playRandom() {
-    const randomIndex = Math.floor(Math.random() * track_list.length);
 
-    let randomTrack = track_list[randomIndex];
 
-    if (isPlaying == false) {
-        outPlayButton.name = 'pause';
-        curr_track.src = randomTrack.path;
-        playTrack()
-    } else {
-        outPlayButton.name = 'play';
-        pauseTrack()
-    }
-}
+
+
+
+
+
+
+// function playTrack() {
+//     curr_track.play();
+//     isPlaying = true;
+
+//     nowPlayingIcon.src = trackImg;      // change bottom left icon
+//     nowPlayingTitle.innerHTML = trackName;      // change bottom left icon
+// }
+
+// function pauseTrack() {
+//     curr_track.pause();
+//     isPlaying = false;
+// }
+
+// out buttons
+
+
+// function playRandom() {
+//     const randomIndex = Math.floor(Math.random() * track_list.length);
+
+//     let randomTrack = track_list[randomIndex];
+
+//     if (isPlaying == false) {
+//         outPlayButton.name = 'pause';
+//         curr_track.src = randomTrack.path;
+//         playTrack()
+//     } else {
+//         outPlayButton.name = 'play';
+//         pauseTrack()
+//     }
+// }
 
 // function playpauseTrack() {
 //     // Switch between playing and pausing
